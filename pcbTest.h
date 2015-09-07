048847c32728f9c9b1752daa10b8e6ef15f36de7
Charlotte Rose Hamilton <charlottesemail@icloud.com>
2015-09-07 14:49:54 -0400

    creating the test
    Creating test files for the pcb.

---
+++ pcbTest.h
@@ -0,0 +1,19 @@
+#ifndef PCBTEST_H
+#define PCBTEST_H
+
+/**************
+*   This test will re turn all true if
+*   every element passes. if 1 fails
+*   an error message occurs and the program will not run.
+*
+*   Charlotte Rose Hamilton
+*************************/
+class pcbTest{
+
+    pcbTest());
+    ~pcbTest();
+    bool
+
+
+};
+#endif // PCBTEST_H
